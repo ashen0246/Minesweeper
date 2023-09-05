d7209588cff83639d82194a3102cfcf9daf31df0
Andy Shen <alshen@usc.edu>
2023-09-05 01:44:35 -0700

    Got grid, text, and buttons all set in the right place

--- app/src/main/java/com/example/minesweeper/MainActivity.java
+++ app/src/main/java/com/example/minesweeper/MainActivity.java
@@ -3,11 +3,85 @@ package com.example.minesweeper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.support.v7.widget.GridLayout;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.view.View;
+import android.content.res.Resources;
+import android.content.pm.ActivityInfo;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    // save the TextViews of all cells in an array, so later on,
+    // when a TextView is clicked, we know which cell it is
+    private ArrayList<TextView> cell_tvs;
+
+    private static final int COLUMN_COUNT = 10;
+    private static final int ROW_COUNT = 12;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //No rotation of screen
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
+
+        cell_tvs = new ArrayList<TextView>();
+
+        GridLayout grid = (GridLayout) findViewById(R.id.field);
+        for (int i = 0; i < ROW_COUNT; i++) {
+            for (int j=0; j < COLUMN_COUNT; j++) {
+                TextView tv = new TextView(this);
+                tv.setHeight( dpToPixel(32) );
+                tv.setWidth( dpToPixel(32) );
+                tv.setTextSize(24);
+                tv.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
+                tv.setTextColor(Color.GRAY);
+                tv.setBackgroundColor(Color.GRAY);
+                tv.setOnClickListener(this::onClickTV);
+
+                GridLayout.LayoutParams lp = new GridLayout.LayoutParams();
+                lp.setMargins(dpToPixel(2), dpToPixel(2), dpToPixel(2), dpToPixel(2));
+                lp.rowSpec = GridLayout.spec(i);
+                lp.columnSpec = GridLayout.spec(j);
+
+                grid.addView(tv, lp);
+
+                cell_tvs.add(tv);
+            }
+        }
+    }
+
+    public void onClickTV(View view){
+        TextView tv = (TextView) view;
+        int n = findIndexOfCellTextView(tv);
+        int i = n/COLUMN_COUNT;
+        int j = n%COLUMN_COUNT;
+        tv.setText(String.valueOf(i)+String.valueOf(j));
+        if (tv.getCurrentTextColor() == Color.GRAY) {
+            tv.setTextColor(Color.GREEN);
+            tv.setBackgroundColor(Color.parseColor("lime"));
+        }else {
+            tv.setTextColor(Color.GRAY);
+            tv.setBackgroundColor(Color.LTGRAY);
+        }
+    }
+
+
+    private int findIndexOfCellTextView(TextView tv) {
+        for (int n=0; n<cell_tvs.size(); n++) {
+            if (cell_tvs.get(n) == tv)
+                return n;
+        }
+        return -1;
+    }
+
+    private int dpToPixel(int dp) {
+        float density = Resources.getSystem().getDisplayMetrics().density;
+        return Math.round(dp * density);
     }
 }
